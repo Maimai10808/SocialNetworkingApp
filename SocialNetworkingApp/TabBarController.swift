e6d103b70d3c3cde14234c6698b0edf541403f34
Maimai10808 <331686549@qq.com>
2025-07-23 19:43:02 +0800

    5. Refactor TabBar Controller

--- SocialNetworkingApp/TabBarController.swift
+++ SocialNetworkingApp/TabBarController.swift
@@ -6,3 +6,36 @@
 //
 
 import Foundation
+import UIKit
+
+class TabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let mainStoryboard        = UIStoryboard(name: "Main", bundle: nil)
+        let homeViewController    = mainStoryboard.instantiateViewController(withIdentifier: "HomeViewController")
+        homeViewController.tabBarItem.image    = UIImage(systemName: "house")
+        homeViewController.tabBarItem.title    = "Home"
+        
+        let postStoryboard        = UIStoryboard(name: "Post", bundle: nil)
+        let postViewController    = postStoryboard.instantiateViewController(withIdentifier: "PostViewController")
+        postViewController.tabBarItem.image    = UIImage(systemName: "paperplane")
+        postViewController.tabBarItem.title    = "Post"
+        
+        let accountStoryboard     = UIStoryboard(name: "Account", bundle: nil)
+        let accountViewController = accountStoryboard.instantiateViewController(withIdentifier: "AccountViewController")
+        accountViewController.tabBarItem.image = UIImage(systemName: "person")
+        accountViewController.tabBarItem.title = "Account"
+        
+        viewControllers = [homeViewController, postViewController, accountViewController]
+         
+    }
+    
+    
+}
+
+
+
+
+
