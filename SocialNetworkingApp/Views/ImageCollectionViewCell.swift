3e1c8b3beea2f8fd35b06b9dd7c32cb600e8b001
Maimai10808 <331686549@qq.com>
2025-07-23 08:56:55 +0800

    2. UICollectionView Cell

---
+++ SocialNetworkingApp/Views/ImageCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  ImageCollectionViewCell.swift
+//  SocialNetworkingApp
+//
+//  Created by mac on 7/23/25.
+//
+
+import UIKit
+
+class ImageCollectionViewCell: UICollectionViewCell {
+    
+}
